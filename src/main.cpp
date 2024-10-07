f07b733a8213fc274155b17505909e8beb646eb3
SoifaAlpaca <sm.inacio@campus.fct.unl.pt>
2024-10-07 15:13:37 +0100

    added-bread

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 
+// NTC XXXX Thermisthor
+// fotmula for temp in Kelvin
+
 #define IN_NTC 4
 #define IN_LM35 2
+#define VDD 3.3
 
 void setup()
 {
+  int NTC_read;
+  int LM35_read;
+  float NTC_AFEOut;
+  float LM35_AFEOut;
   pinMode(IN_NTC, INPUT);  // Set the ADC pin as input
   pinMode(IN_LM35, INPUT); // Set the ADC pin as input
 
@@ -14,14 +22,14 @@ void setup()
 
 void loop()
 {
-  int NTC_VALUE = analogRead(IN_NTC);
-  int LM35_VALUE = analogRead(IN_LM35);
-  float voltage_NTC = NTC_VALUE * (3.3 / 4095);   // 3.3V is the reference voltage of ESP32 and 4095 is the maximum value of ADC (2^12 - 1)
-  float voltage_LM35 = LM35_VALUE * (3.3 / 4095); // 3.3V is the reference voltage of ESP32 and 4095 is the maximum value of ADC (2^12 - 1)
+  int NTC_read = analogRead(IN_NTC);
+  int LM35_read = analogRead(IN_LM35);
+  float NTC_AFEOut = NTC_read * (3.3 / 4095);   // 3.3V is the reference voltage of ESP32 and 4095 is the maximum value of ADC (2^12 - 1)
+  float LM35_AFEOut = LM35_read * (3.3 / 4095); // 3.3V is the reference voltage of ESP32 and 4095 is the maximum value of ADC (2^12 - 1)
   Serial.print("\nNTC Voltage: ");
-  Serial.println(voltage_NTC);
+  Serial.println(NTC_AFEOut);
   Serial.print("\nLM35 Voltage: ");
-  Serial.println(voltage_LM35);
+  Serial.println(LM35_AFEOut);
 
   delay(1000);
 }
